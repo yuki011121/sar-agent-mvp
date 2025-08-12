3ebfdfb7523b5a8c97b034af475e620f26f8ac08
RandomCyberCoder <emmanueldelgado74@gmail.com>
2025-08-11 17:36:25 -0700

    updated to use RedisBus class

--- agents/history/main.py
+++ agents/history/main.py
@@ -11,20 +11,25 @@ from datetime import datetime, timezone
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+#for pub/sub for redis
+from shared.a2a_envelope import wrap_envelope
+from shared.redis_bus import RedisBus
+
+#using tools, mcp
 
 load_dotenv()
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
-AGENT_VERSION = "history-agent-v1.0"
+AGENT_NAME = "history-agent"
 STREAM_NAME_IN = "history.in.raw"
 STREAM_NAME_OUT = "history.out.raw"
-AGENT_VERSION = "history-agent-v1.0"
+AGENT_VERSION = "1.1"
 OPENAI_KEY = os.getenv("OPENAI_KEY", None)
 ISRID_COLUMNS = ['Data.Source', 'Incident.Outcome', 'Terrain', 'Subject.Category', 'Subject.Activity', 'Age',
                            'Sex', 'Subject.Status']
 TOP_K_MATCHES = 3
 last_msg_ID = None
 vectorizer = TfidfVectorizer()
-
+# logger = logging.getLogger(__name__)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -46,24 +51,24 @@ except redis.exceptions.ConnectionError as e:
     logging.error(f"Could not connect to Redis: {e}")
     exit(1)
 
-def redis_input():
-    global last_msg_ID
-    ID_INDEX = 0
-    MSG_DATA_INDEX = 1
-    logging.info("Blocking until a new message is found")
-    #reads the oldest message (switch to $ for newest), limit 1 message to fetch, block forever until
-    #a message is received 
-    input_received = redis_client.xread({STREAM_NAME_IN: ('$' if last_msg_ID is None else last_msg_ID)}, count=1, block=0)
-    #input_received is an array arrays that contain a key (stream name)
-    # and an array of tuples (ID, field-value pairs)
-    stream_data = input_received[0]
-    msgs_read = stream_data[1]
-    #there is only one msg that is read
-    msg = msgs_read[0]
-    last_msg_ID = msg[ID_INDEX]
-    msg_data = msg[MSG_DATA_INDEX]
-    logging.info("Read message and parsed correctly")
-    return msg_data
+# def redis_input():
+#     global last_msg_ID
+#     ID_INDEX = 0
+#     MSG_DATA_INDEX = 1
+#     logging.info("Blocking until a new message is found")
+#     #reads the oldest message (switch to $ for newest), limit 1 message to fetch, block forever until
+#     #a message is received 
+#     input_received = redis_client.xread({STREAM_NAME_IN: ('$' if last_msg_ID is None else last_msg_ID)}, count=1, block=0)
+#     #input_received is an array arrays that contain a key (stream name)
+#     # and an array of tuples (ID, field-value pairs)
+#     stream_data = input_received[0]
+#     msgs_read = stream_data[1]
+#     #there is only one msg that is read
+#     msg = msgs_read[0]
+#     last_msg_ID = msg[ID_INDEX]
+#     msg_data = msg[MSG_DATA_INDEX]
+#     logging.info("Read message and parsed correctly")
+#     return msg_data
 
 def redis_output(standardized_output):
     redis_client.xadd(STREAM_NAME_OUT, {"data": json.dumps(standardized_output)})
@@ -190,34 +195,49 @@ def prompt_llm(matches: pd.DataFrame, query: dict):
     return summary, actions
 
 def main():
+    logging.info(f"Initializing {AGENT_NAME}...")
+
+    try:
+        bus = RedisBus(REDIS_URL)
+        subGen = bus.subscribe("history", "history-agent", [STREAM_NAME_IN])
+    except Exception as e:
+        logging.critical(f"Failed to connect to Redis, cannot start agent. Error: {e}")
+        return 
+
+
+
     logging.info("Reading Isirid Dataset")
     #key is the input name from redis json and value is column name in the isrid csv
     isrid = pd.read_csv('agents/history/isrid2searches4calpoly_output.csv', index_col=0)
     concatenated_rows = isrid.apply(lambda row: " ".join(row.astype(str)), axis=1)
     vectorized_rows = vectorizer.fit_transform(concatenated_rows)
     logging.info("Start redis channel listening loop")
-    while True:
-        message_read = redis_input()
 
-        matches = find_match(isrid, vectorized_rows, message_read)
+    for message_read in subGen:
+        # # message_read = redis_input()
+        # print("got here")
+        # print(message_read.payload)
+        # # continue
+        matches = find_match(isrid, vectorized_rows, message_read.payload)
         try:
-            summary, actions = prompt_llm(matches, message_read)
-            metadata = {
-                "agent_name": AGENT_VERSION,
-                "timestamp_utc": datetime.now(timezone.utc).isoformat(),
-                "source": {
-                    "summary-llm": "gpt-4.1-nano"
-                }
-            }
-            standardized_output = {
-                "metadata": metadata,
+            summary, actions = prompt_llm(matches, message_read.payload)
+
+            payload = {
                 "summary" : summary,
                 "actions" : actions
             }
-            redis_output(standardized_output)
-            print(standardized_output)
+
+            message_to_publish = wrap_envelope(
+                payload=payload,
+                source_name=AGENT_NAME,
+                source_version=AGENT_VERSION,
+                target_stream=STREAM_NAME_OUT
+            )
+
+            bus.publish(message_to_publish)
+            print(message_to_publish)
         except Exception as e:
-            logging.error(f"Error querying llm: {e}")
+            logging.error(f"Error querying llm or publishing to redis: {e}")
         
 
 
